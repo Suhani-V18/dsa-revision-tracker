b4d491194494d0763b363ca326adcd2b18d4f11b
Suhani-V18 <suhanivishwakarma185@gmail.com>
2026-07-14 15:50:10 +0530

    Add server url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const createAttempt = (data) => axios.post(`${API_BASE}/attempts`, data);
 export const getScores = () => axios.get(`${API_BASE}/attempts/scores`);
